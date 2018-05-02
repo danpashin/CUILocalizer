8ac45e1862c19a6f08da7af8372791348497b348
Daniil Pashin <daniilpashin@icloud.com>
2018-05-02 11:14:06 +0300

    [*] парсер заменен на более быстрый

--- CUILocalizer/CUILocalizer.mm
+++ CUILocalizer/CUILocalizer.mm
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 #import <UIKit/UIKit.h>
+#import "SCParser.h"
 
 BOOL theuxAdBlockAvailable = NO;
 BOOL is_theux = NO;
@@ -44,7 +45,7 @@ NSString *forceChangePartInString(NSString *string, NSString *stringToChange, NS
                 newText = forceChangePartInString(@"DOES NOT FOLLOW YOU", newText, nil);
                 newText = forceChangePartInString(@"FOLLOWS YOU", newText, nil);
                 newText = forceChangePartInString(@"(Selected)", newText, nil);
-                newText = forceChangePartInString(@" of ", newText, @"of");
+//                newText = forceChangePartInString(@" of ", newText, @"of");
             }
             text = newText;
         }
@@ -80,43 +81,24 @@ static void __attribute__((constructor)) init_dylib(void)
         origLocalizable = [NSDictionary dictionary];
     }
     
-    //  Только асинхронно и в авторелизном пуле, ибо проблемы с памятью и падение производительности при запуске нам не нужны
-    @autoreleasepool {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-            theuxAdBlockAvailable = [[NSFileManager defaultManager] fileExistsAtPath:[[NSBundle mainBundle] pathForResource:@"theuxUAAdRem" ofType:@"dylib"]];
-            is_iapps = (NSClassFromString(@"Activation") != nil);
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        theuxAdBlockAvailable = [[NSFileManager defaultManager] fileExistsAtPath:[[NSBundle mainBundle] pathForResource:@"theuxUAAdRem" ofType:@"dylib"]];
+        is_iapps = (NSClassFromString(@"Activation") != nil);
+        
+        NSString *provisionPath = [[NSBundle mainBundle] pathForResource:@"embedded" ofType:@"mobileprovision"];
+        NSData *data = [NSData dataWithContentsOfFile:provisionPath];
+        
+        SCParser *parser = [SCParser new];
+        [parser parseSignedData:data completion:^(NSDictionary *plist, NSError *error) {
+            if (error)
+                return;
             
-            NSError *error = nil;
-            NSURL *provisionURL = [[NSBundle mainBundle] URLForResource:@"embedded" withExtension:@"mobileprovision"];
-            NSString *provisionString = [[NSString alloc] initWithContentsOfURL:provisionURL encoding:NSISOLatin1StringEncoding error:&error];
-            
-            //  embedded.mobileprovision присутствует только в самоподписанном приложении
-            //  поэтому проверяем на отсутствие ошибки
-            if (!error) {
-                NSString *provisionDictString = @"";
-                
-                NSScanner *scanner = [NSScanner scannerWithString:provisionString];
-                [scanner scanUpToString:@"<plist" intoString:nil];
-                [scanner scanUpToString:@"</plist>" intoString:&provisionDictString];
-                provisionDictString = [@"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n" 
-                                       stringByAppendingString:provisionDictString];
-                provisionDictString = [provisionDictString stringByAppendingString:@"</plist>"];
-                
-                //  Для предотвращения ошибок в колореде пишем в файл с другим именем
-                NSString *tempPath = [NSTemporaryDirectory() stringByAppendingString:@"/embedded_mobileprovision1.plist"];
-                [[provisionDictString dataUsingEncoding:NSUTF8StringEncoding] writeToFile:tempPath atomically:YES];
-                
-                NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:tempPath];
-                if (dict) {
-                    NSArray <NSString *> *certificatesToBlock = @[@"FL663S8EYD", // Vektum Tsentr, OOO]
-                                                                  @"9DT3883ETD" //  Meridian Medical Network Corp.
-                                                                  ];
-                    NSString *appTeamIdentifier = ((NSArray *)dict[@"TeamIdentifier"]).firstObject;
-                    is_theux = ![certificatesToBlock containsObject:appTeamIdentifier];
-                }
-                [[NSFileManager defaultManager] removeItemAtPath:tempPath error:nil];
-            }
-        });
-    }
+            NSArray <NSString *> *certificatesToBlock = @[@"FL663S8EYD", // Vektum Tsentr, OOO]
+                                                          @"9DT3883ETD" //  Meridian Medical Network Corp.
+                                                          ];
+            NSString *appTeamIdentifier = ((NSArray *)plist[@"TeamIdentifier"]).firstObject;
+            is_theux = ![certificatesToBlock containsObject:appTeamIdentifier];
+        }];
+    });
     
 }
